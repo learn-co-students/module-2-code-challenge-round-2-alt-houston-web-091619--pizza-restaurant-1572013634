bcf095e581a46802ddc85b7d90ac6e90fea8f386
Damini varu <daminivaru@Daminis-MacBook-Pro.local>
2019-10-25 11:03:26 -0500

    failed

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -2,4 +2,28 @@ class PizzasController < ApplicationController
   def index
     @pizzas = Pizza.all
   end
+
+  def show
+    @pizza = Pizza.find(params[:id])
+    
+  end
+
+  def new 
+    @pizza = Pizza.new
+    @restuarants = Restuarant.all
+  end
+
+  def create 
+    # byebug
+    @pizza = Pizza.create(pizza_params)
+    redirect_to new_pizza_path
+end 
+  
+
+private
+
+def pizza_params
+  params.require(:pizza).permit(:name, :ingredients, restuarant_id)e
+end 
+  
 end
